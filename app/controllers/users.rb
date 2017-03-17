6e3f4527fb9f78c51a68d6397f664d3d340a0a65
Joseph Huang <huangx5j@gmail.com>
2017-03-16 19:55:57 -0700

    Added comments for user routes

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,13 +1,15 @@
-
+# display a list of all users
 get '/users' do
 	@users = User.all
 	erb :index
 end
 
+# create a new user
 get '/users/new' do
 	erb :'/users/new'
 end
 
+# register/create a new user
 post '/users' do
 	@user = User.new(params[:user])
 	if @user.save
@@ -18,17 +20,21 @@ post '/users' do
 	end
 end
 
+# display a specific user
 get '/users/:id' do
 	@user = User.find_by(id: params[:id])
 	erb :'/users/show'
 end
 
+# ===========================================
 
+# return a form for editing a user
 get '/users/:id/edit' do
 	@user = User.find_by(id: params[:id])
 	erb :'/users/edit'
 end
 
+# update a specific user
 put '/users/:id' do
 	@user = User.find_by(id: params[:id])
 	@user.assign_attributes(params[:user])
@@ -39,6 +45,7 @@ put '/users/:id' do
 	end
 end
 
+# delete a specific user
 delete '/users/:id' do
 	@user.destroy
 	redirect '/'
