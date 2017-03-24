111ca9d02855c26076a97fd30512c5ba80973136
Joseph Huang <email": null>
2017-03-23 17:48:04 -0700

    Add bing image api which search for an image and add to index.erb

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,43 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  //Declare variables
+  var $searchButton = $('#searchButton');
+  //add a colon to the beginning of your AppId string
+  var appId = ':fbe7a5147c4946a3a052a400859992a3';
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  //Function to get images
+  function getImage() {
+    //base64 encode the AppId
+    var azureKey = btoa(appId);
+    //get the value from the search box
+    var $searchQuery = $('#searchBox').val();
+    //Create the search string
+    var myUrl = "https://api.cognitive.microsoft.com/bing/v5.0/images/search" + "?q=" + $searchQuery;
+    //Make post request to bing
+    $.ajax({
+      method: 'post',
+      url: myUrl,
+      //Set headers to authorize search with Bing
+      headers: {
+        // 'Ocp-Apim-Subscription-Key': azureKey
+        'Ocp-Apim-Subscription-Key': 'fbe7a5147c4946a3a052a400859992a3'
+      },
+      success: function(data) {
+        // for debugging & transversing the data objects
+        // window.imageData = data;
+        var imgLink = data.value[0].thumbnailUrl;
+        var image = $('<img></img>');
+        image.attr('src', imgLink);
+        $('#output').html(image);
+      },
+      failure: function(err) {
+        console.error(err);
+      }
+    });
+  };
+  //Trigger function when button is clicked
+  $searchButton.click(function(e) {
+    e.preventDefault();
+    getImage();
+  });
 });
+
